9403de321cf3da6da1870a1b2d0ebb9bc1dcbb62
cagatay yilmaz <mustafa.cagatay.yilmaz@gmail.com>
2023-04-16 16:32:55 +0300

    Updated jdk

--- src/main/java/com/cagatay/githubcicdactions/GithubCicdActionsApplication.java
+++ src/main/java/com/cagatay/githubcicdactions/GithubCicdActionsApplication.java
@@ -2,12 +2,20 @@ package com.cagatay.githubcicdactions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GithubCicdActionsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GithubCicdActionsApplication.class, args);
-	}
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "welcome";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCicdActionsApplication.class, args);
+    }
 
 }
